c5486a7b0df96257242c6d96c217586882e3a862
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-09-05 18:25:21 +0100

    app comp subscribes to service and sends data to the child comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,14 @@ import { WikipediaService } from './wikipedia.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  pages = [];
   constructor(private WikipediaServ: WikipediaService) {}
   //constructor(){const wiki = new WikipediaService()} -> [NOT Recommended]
   onTerm(term: string) {
-    this.WikipediaServ.search(term).subscribe((response) => {
-      console.log(response);
+    this.WikipediaServ.search(term).subscribe((response: any) => {
+      // console.log(response);
+      this.pages = response.query.search;
+      console.log(this.pages);
       //pass in a callback function, it's going t obe called with whatever resp we get from the Wikipedia API.
     });
     // const results = this.WikipediaServ.search(term);
--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-list',
   templateUrl: './page-list.component.html',
-  styleUrls: ['./page-list.component.css']
+  styleUrls: ['./page-list.component.css'],
 })
 export class PageListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  @Input() pages: any[] = [];
+  ngOnInit(): void {}
 }
